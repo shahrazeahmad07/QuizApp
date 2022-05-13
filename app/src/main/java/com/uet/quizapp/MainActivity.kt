f76593ff0012d186df68b0d600e259d69f6b85d1
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2022-05-13 17:49:41 +0500

    New Activity created and linked with start button.

--- app/src/main/java/com/uet/quizapp/MainActivity.kt
+++ app/src/main/java/com/uet/quizapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.uet.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btnStart)
+        val etName : EditText = findViewById(R.id.etName)
+
+        btnStart.setOnClickListener {
+            if (etName.text.isEmpty()) {
+                Toast.makeText(this, "Please Enter your Name.", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
     }
 }
